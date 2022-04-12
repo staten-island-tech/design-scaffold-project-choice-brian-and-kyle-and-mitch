e5e632ff3c7b425e52874db9ea10c674fdddf268
bluo0333 <69543418+bluo0333@users.noreply.github.com>
2022-04-12 00:43:01 -0400

    1

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,27 @@
 import "./css/styles.css";
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+document.getElementById("button1").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#top" });
+});
+document.getElementById("button2").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#sec-2" });
+});
+document.getElementById("button3").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#sec-3" });
+});
+document.getElementById("button4").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#sec-4" });
+});
+document.getElementById("button5").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#sec-5" });
+});
+document.getElementById("button6").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#sec-6" });
+});
+document.getElementById("button7").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#sec-7" });
+});
