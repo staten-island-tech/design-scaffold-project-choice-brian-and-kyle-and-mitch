fd717e2a7e68e5258463caf353c72cea12f74938
bluo0333 <69543418+bluo0333@users.noreply.github.com>
2022-05-12 18:44:09 -0400

    1

--- vite-project/main.js
+++ vite-project/main.js
@@ -25,3 +25,13 @@ document.getElementById("button6").addEventListener("click", function () {
 document.getElementById("button7").addEventListener("click", function () {
   gsap.to(window, { duration: 1, scrollTo: "#top7" });
 });
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("secondary-theme")) {
+    document.body.classList.add("primary-theme");
+    document.body.classList.remove("secondary-theme");
+  } else {
+    document.body.classList.add("secondary-theme");
+    document.body.classList.remove("primary-theme");
+  }
+});
